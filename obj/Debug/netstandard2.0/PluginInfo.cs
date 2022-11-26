4c16b89b58e1fc7f69d0ac13544373e0bc92a040
space-commits <57108281+space-commits@users.noreply.github.com>
2022-11-26 02:42:24 +0000

    Removed Unnecessary Files From Repo

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RealismMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "RealismMod";
-        public const string PLUGIN_NAME = "RealismMod";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- obj/Release/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace AttatchmentPatch
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "AttatchmentPatch";
-        public const string PLUGIN_NAME = "AttatchmentPatch";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
