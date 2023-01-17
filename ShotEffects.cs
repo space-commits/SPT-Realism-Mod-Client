ed9d0ffbe4e430c58cba133fc802a0ebccd4b331
space-commits <57108281+space-commits@users.noreply.github.com>
2023-01-17 16:59:20 +0000

    Fixed issue of audio changing with headset
    Fixed deafening effect changing after equiping/unequiping headset. It was a BSG bug :)

--- ShotEffects.cs
+++ ShotEffects.cs
@@ -136,7 +136,9 @@ namespace RealismMod
             {
                 EquipmentClass equipment = (EquipmentClass)AccessTools.Property(typeof(Player), "Equipment").GetValue(__instance);
                 Plugin.EarProtectionFactor = HeadsetDeafFactor(equipment) * HelmDeafFactor(equipment);
+                Logger.LogWarning("HasHeadSet = " + Plugin.HasHeadSet);
                 Logger.LogWarning("Earprotection = " + Plugin.EarProtectionFactor);
+                Logger.LogWarning("===========================================");
             }
 
         }
@@ -144,72 +146,42 @@ namespace RealismMod
 
     public static class Deafening
     {
-/*        public static float CompressorVolume = 0f;*/
+
         public static float Volume = 0f;
         public static float Distortion = 0f;
         public static float VignetteDarkness = 0f;
 
-/*        public static float CompressorVolumeLimit = 0f;*/
         public static float VolumeLimit = -25f;
         public static float DistortionLimit = 200f;
         public static float VignetteDarknessLimit = 80f;
 
-/*        public static float CompressorVolumeIncreaseRate = 0f;*/
         public static float VolumeIncreaseRate = 0.07f;
         public static float DistortionIncreaseRate = 0.15f;
         public static float VignetteDarknessIncreaseRate = 0.1f;
 
-/*        public static float CompressorVolumeResetRate = 0f;*/
         public static float VolumeResetRate = 0.05f;
         public static float DistortionResetRate = 0.3f;
         public static float VignetteDarknessResetRate = 0.5f;
 
         public static void DoDeafening()
         {
-            if (Plugin.HasHeadSet == false)
-            {
-                if (Plugin.IsFiring == true)
-                {
-                    Plugin.Vignette.enabled = true;
-                    VignetteDarkness = Mathf.Clamp(VignetteDarkness += VignetteDarknessIncreaseRate * Plugin.EarProtectionFactor, 0.0f, VignetteDarknessLimit);
-                    Volume = Mathf.Clamp(Volume -= VolumeIncreaseRate * Plugin.EarProtectionFactor, VolumeLimit, 0.0f);
-                    Distortion = Mathf.Clamp(Distortion += DistortionIncreaseRate * Plugin.EarProtectionFactor, 0.0f, DistortionLimit);
-                }
-                else
-                {
-                    VignetteDarkness = Mathf.Clamp(VignetteDarkness -= VignetteDarknessResetRate, 0.0f, VignetteDarknessLimit);
-                    Volume = Mathf.Clamp(Volume += VolumeResetRate, VolumeLimit, 0.0f);
-                    Distortion = Mathf.Clamp(Distortion -= DistortionResetRate, 0.0f, DistortionLimit);
-                }
 
-                Plugin.Vignette.darkness = VignetteDarkness;
-                Singleton<BetterAudio>.Instance.Master.SetFloat("GunsVolume", Volume + Plugin.GunsVolume);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("OcclusionVolume", Volume + Plugin.MainVolume);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("EnvironmentVolume", Volume + Plugin.MainVolume);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("AmbientVolume", Volume + Plugin.AmbientVolume);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("AmbientOccluded", Volume + Plugin.AmbientOccluded);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorDistortion", Distortion + Plugin.CompressorDistortion);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorResonance", Distortion + Plugin.CompressorResonance);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorLowpass", Distortion + Plugin.CompressorLowpass);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("Compressor", Distortion + Plugin.Compressor);
+            if (Plugin.IsFiring == true)
+            {
+                Plugin.Vignette.enabled = true;
+                VignetteDarkness = Mathf.Clamp(VignetteDarkness += VignetteDarknessIncreaseRate * Plugin.EarProtectionFactor, 0.0f, VignetteDarknessLimit);
+                Volume = Mathf.Clamp(Volume -= VolumeIncreaseRate * Plugin.EarProtectionFactor, VolumeLimit, 0.0f);
+                Distortion = Mathf.Clamp(Distortion += DistortionIncreaseRate * Plugin.EarProtectionFactor, 0.0f, DistortionLimit);
             }
             else
             {
-                if (Plugin.IsFiring == true)
-                {
+                VignetteDarkness = Mathf.Clamp(VignetteDarkness -= VignetteDarknessResetRate, 0.0f, VignetteDarknessLimit);
+                Volume = Mathf.Clamp(Volume += VolumeResetRate, VolumeLimit, 0.0f);
+                Distortion = Mathf.Clamp(Distortion -= DistortionResetRate, 0.0f, DistortionLimit);
+            }
 
-                    Plugin.Vignette.enabled = true;
-                    VignetteDarkness = Mathf.Clamp(VignetteDarkness += VignetteDarknessIncreaseRate * Plugin.EarProtectionFactor, 0.0f, VignetteDarknessLimit);
-                    Volume = Mathf.Clamp(Volume -= VolumeIncreaseRate * Plugin.EarProtectionFactor, VolumeLimit, 0.0f);
-                    Distortion = Mathf.Clamp(Distortion += DistortionIncreaseRate * Plugin.EarProtectionFactor, 0.0f, DistortionLimit);
-                }
-                else
-                {
-                    VignetteDarkness = Mathf.Clamp(VignetteDarkness -= VignetteDarknessResetRate, 0.0f, 100.0f);
-                    Volume = Mathf.Clamp(Volume += VolumeResetRate, VolumeLimit, 0.0f);
-                    Distortion = Mathf.Clamp(Distortion -= DistortionResetRate, 0.0f, DistortionLimit);
-                }
-
+            if (Volume != 0 || Distortion != 0)
+            {
                 Plugin.Vignette.darkness = VignetteDarkness;
                 Singleton<BetterAudio>.Instance.Master.SetFloat("GunsVolume", Volume + Plugin.GunsVolume);
                 Singleton<BetterAudio>.Instance.Master.SetFloat("OcclusionVolume", Volume + Plugin.MainVolume);
@@ -217,9 +189,23 @@ namespace RealismMod
                 Singleton<BetterAudio>.Instance.Master.SetFloat("AmbientVolume", Volume + Plugin.AmbientVolume);
                 Singleton<BetterAudio>.Instance.Master.SetFloat("AmbientOccluded", Volume + Plugin.AmbientOccluded);
                 Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorResonance", Distortion + Plugin.CompressorResonance);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorLowpass", Distortion + Plugin.CompressorLowpass);
-                Singleton<BetterAudio>.Instance.Master.SetFloat("Compressor", Volume + Plugin.Compressor);
+                Singleton<BetterAudio>.Instance.Master.SetFloat("Compressor", Distortion + Plugin.Compressor);
+                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorDistortion", Distortion + Plugin.CompressorDistortion);
+                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorLowpass", Distortion + Plugin.CompressorDistortion);
+
+                /*   if (Plugin.HasHeadSet == false)
+                   {
+
+                   }
+                   else
+                   {
+                       Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorLowpass", Distortion + Plugin.CompressorLowpass);
+                   }*/
             }
+      /*      else
+            {
+                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorLowpass", Plugin.CompressorLowpass);
+            }*/
         }
     }
 
@@ -233,19 +219,87 @@ namespace RealismMod
         [PatchPostfix]
         private static void PatchPostfix(GClass2015 template)
         {
+            Logger.LogWarning("=====================6======================");
             Logger.LogWarning("SetCompressor");
             bool flag = template != null;
+
+            Logger.LogWarning("Template is Not Null? " + flag);
 
             Plugin.MainVolume = flag ? template.DryVolume : 0f;
             Plugin.Compressor = flag ? template.CompressorVolume : -80f;
             Plugin.AmbientVolume = flag ? template.AmbientVolume : 0f;
             Plugin.AmbientOccluded = flag ? (template.AmbientVolume - 15f) : -5f;
-            Plugin.GunsVolume = flag ? (template.DryVolume - 100f) : 0f;
+            Plugin.GunsVolume = flag ? (template.DryVolume - 20f) : 0f;
 
-            Plugin.CompressorDistortion = flag ? template.Distortion : 0f;
-            Plugin.CompressorResonance = flag ? template.Resonance : 0f;
-            Plugin.CompressorLowpass = flag ? template.LowpassFreq : 0f;
+            Plugin.CompressorDistortion = flag ? template.Distortion : 0.277f; //0.277
+            Plugin.CompressorResonance = flag ? template.Resonance : 2.47f; //2.47
+            Plugin.CompressorLowpass = flag ? template.LowpassFreq : 22000f;
 
+            if (flag == false)
+            {
+                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorCutoff", 245f);
+                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorTreshold", -20f);
+                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorMakeup", 10f);
+                Singleton<BetterAudio>.Instance.Master.SetFloat("CompressorAttack", 35f);
+            }
+
+
+            float GunsVolume;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("GunsVolume", out GunsVolume);
+            Logger.LogWarning("GunsVolume " + GunsVolume);
+
+            float OcclusionVolume;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("OcclusionVolume", out OcclusionVolume);
+            Logger.LogWarning("OcclusionVolume " + OcclusionVolume);
+
+            float EnvironmentVolume;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("EnvironmentVolume", out EnvironmentVolume);
+            Logger.LogWarning("EnvironmentVolume " + EnvironmentVolume);
+
+            float AmbientVolume;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("AmbientVolume", out AmbientVolume);
+            Logger.LogWarning("AmbientVolume " + AmbientVolume);
+
+            float AmbientOccluded;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("AmbientOccluded", out AmbientOccluded);
+            Logger.LogWarning("AmbientOccluded " + AmbientOccluded);
+
+            float CompressorResonance;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("CompressorResonance", out CompressorResonance);
+            Logger.LogWarning("CompressorResonance " + CompressorResonance);//////////////
+
+            float Compressor;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("Compressor", out Compressor);
+            Logger.LogWarning("Compressor " + Compressor);
+
+            float CompressorDistortion;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("CompressorDistortion", out CompressorDistortion);
+            Logger.LogWarning("CompressorDistortion " + CompressorDistortion);///////////////
+
+            float CompressorLowpass;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("CompressorLowpass", out CompressorLowpass);
+            Logger.LogWarning("CompressorLowpass " + CompressorLowpass);
+
+
+            float CompressorCutoff;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("CompressorCutoff", out CompressorCutoff);
+            Logger.LogWarning("CompressorCutoff " + CompressorCutoff);
+
+            float CompressorTreshold;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("CompressorTreshold", out CompressorTreshold);
+            Logger.LogWarning("CompressorTreshold " + CompressorTreshold);
+
+            float CompressorMakeup;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("CompressorMakeup", out CompressorMakeup);
+            Logger.LogWarning("CompressorMakeup " + CompressorMakeup);
+
+            float CompressorAttack;
+            Singleton<BetterAudio>.Instance.Master.GetFloat("CompressorAttack", out CompressorAttack);
+            Logger.LogWarning("CompressorAttack " + CompressorAttack);
+
+
+
+            Logger.LogWarning("=================================");
             Logger.LogWarning("MainVolume " + Plugin.MainVolume);
             Logger.LogWarning("Compressor " + Plugin.Compressor);
             Logger.LogWarning("AmbientVolume " + Plugin.AmbientVolume);
@@ -254,7 +308,7 @@ namespace RealismMod
             Logger.LogWarning("CompressorDistortion " + Plugin.CompressorDistortion);
             Logger.LogWarning("CompressorResonance " + Plugin.CompressorResonance);
             Logger.LogWarning("CompressorLowpass " + Plugin.CompressorLowpass);
-
+            Logger.LogWarning("=================================");
         }
     }
 
