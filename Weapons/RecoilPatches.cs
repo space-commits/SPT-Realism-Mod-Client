fe71b03468fed6419e55a99b301d88d3ae1f5f1d
space-commits <57108281+space-commits@users.noreply.github.com>
2023-09-13 15:11:46 +0100

    tweaks

--- Weapons/RecoilPatches.cs
+++ Weapons/RecoilPatches.cs
@@ -302,8 +302,8 @@ namespace RealismMod
                 __instance.ShotVals[3].Intensity = totalCamRecoil;
                 __instance.ShotVals[4].Intensity = -totalCamRecoil;
 
-                float fovFactor = Singleton<SharedGameSettingsClass>.Instance.Game.Settings.FieldOfView / 70f;
-                float opticLimit = Plugin.IsAiming && Plugin.HasOptic ? 15f * fovFactor : 40f * fovFactor;
+                float fovFactor = (Singleton<SharedGameSettingsClass>.Instance.Game.Settings.FieldOfView / 70f) * Plugin.HRecLimitMulti.Value;
+                float opticLimit = Plugin.IsAiming && Plugin.HasOptic ? 15f * fovFactor: 35f * fovFactor;
                 float shotFactor = 1f;
                 if (weaponClass.WeapClass == "pistol" && RecoilController.ShotCount > 1f && weaponClass.SelectedFireMode == Weapon.EFireMode.fullauto) 
                 {
